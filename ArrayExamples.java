5924f76c21836f0abe88f1c5dfd2f9d8dbc7d7c3
1chig018 <130116127+1chig018@users.noreply.github.com>
2023-04-20 18:47:34 -0700

    checkpoint

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -4,8 +4,10 @@ public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
+    for(int i = 0; i < arr.length/2; i += 1) { //change the loop to half for swapping
+      int reverse = arr[i]; //swapping the ith and ith last elements
       arr[i] = arr[arr.length - i - 1];
+      arr[arr.length - i - 1] = reverse;    
     }
   }
 
@@ -14,9 +16,9 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray; //return the right thing which is newArray not arr
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
